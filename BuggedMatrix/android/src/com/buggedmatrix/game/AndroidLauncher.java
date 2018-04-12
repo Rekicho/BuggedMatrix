2d260fc24af6559c65ff80823051a0f74e65e16a
Rekicho <up201604145@fe.up.pt>
2018-04-12 14:33:05 +0100

    Created libgdx project

---
+++ BuggedMatrix/android/src/com/buggedmatrix/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.buggedmatrix.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.buggedmatrix.game.BuggedMatrix;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BuggedMatrix(), config);
+	}
+}
---
+++ BuggedMatrix/desktop/src/com/buggedmatrix/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.buggedmatrix.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.buggedmatrix.game.BuggedMatrix;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BuggedMatrix(), config);
+	}
+}
